d34531e93d28e88b95dcd70e8933782117d3e77f
Anthony Jones <anthony15jones@gmail.com>
2023-04-29 13:37:35 -0500

    second module

--- diccionario.py
+++ diccionario.py
@@ -8,18 +8,18 @@ session = Session(engine)
 
 while True:
     print('\n....... MENU.......\n ')
-    print('1. visualizar')
-    print('2. Agregar')
-    print('3. Actualizar')
-    print('4. Eliminar')
-    print('5. Buscar')
+    print('1. Mostrar palabras')
+    print('2. Agregar palabra')
+    print('3. Actualizar palabra')
+    print('4. Eliminar palabra')
+    print('5. Buscar palabra')
     print('6. Salir')
-    opcion = int(input('Opcion: '))
+    opcion = int(input('Seleccione una opción: '))
 
     if opcion == 1:
         lista = [i for i in session.scalars(select(Palabra))]
         if len(lista) == 0:
-            print('No hay palabras!')
+            print('¡No hay palabras!')
         else:
             for i in lista:
                 print('-' * 30)
@@ -27,43 +27,43 @@ while True:
 
     elif opcion == 2:
         palabra = Palabra(
-            nombre=input('Nombre: ').lower(),
-            significado=input('Significado: ').lower()
+            nombre=input('Ingrese el nombre de la palabra: ').lower(),
+            significado=input('Ingrese el significado de la palabra: ').lower()
         )
         session.add(palabra)
         session.commit()
-        print('Palabra agregada!')
+        print('¡Palabra agregada!')
 
     elif opcion == 3:
-        nombre = input('Ingrese el nombre de la palabra: ').lower()
+        nombre = input('Ingrese el nombre de la palabra a actualizar: ').lower()
         palabra: Palabra = session.scalar(select(Palabra).where(Palabra.nombre == nombre))
         if palabra:
-            palabra.significado = input('Ingrese el nuevo significado: ')
+            palabra.significado = input('Ingrese el nuevo significado de la palabra: ')
             session.commit()
-            print('Palabra actualizado!')
+            print('¡Palabra actualizada!')
         else:
-            print('La palabra no existe!')
+            print('¡La palabra no existe!')
 
     elif opcion == 4:
-        nombre = input('Ingrese el nombre de la palabra: ').lower()
+        nombre = input('Ingrese el nombre de la palabra a eliminar: ').lower()
         palabra: Palabra = session.scalar(select(Palabra).where(Palabra.nombre == nombre))
         if palabra:
             session.delete(palabra)
             session.commit()
-            print('Palabra eliminada!')
+            print('¡Palabra eliminada!')
         else:
-            print('La palabra no existe!')
+            print('¡La palabra no existe!')
 
     elif opcion == 5:
-        nombre = input('Ingrese el nombre de la palabra: ').lower()
+        nombre = input('Ingrese el nombre de la palabra a buscar: ').lower()
         palabra: Palabra = session.scalar(select(Palabra).where(Palabra.nombre == nombre))
         if palabra:
             print(palabra)
         else:
-            print('La palabra no existe!')
+            print('¡La palabra no existe!')
 
     elif opcion == 6:
         break
 
     else:
-        print("Opcion incorrecta.")
+        print("¡Opción incorrecta!")```
